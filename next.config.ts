e68c2da5e4a5d877a865ba05a150e509f95b27ac
Himanshu Prakash Bohra <55129762+HimanshuPBohra@users.noreply.github.com>
2025-04-04 19:00:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  eslint: {
+    // This will ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
